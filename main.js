898cb9ceb71bc3f596aa315ef06315a4205d30a3
Prabhanjan Misra <44291232+Prabhanjanmisra@users.noreply.github.com>
2023-07-21 17:52:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function refresh(e) {
+    if ($("#button-circle").css("opacity") == 1) {
+        $("#button-circle").addClass("clicked");
+        setTimeout(function () {
+            $("#button-circle").removeClass("clicked");
+        }, 100);
+    }
+
+
+    fetch("https://api.adviceslip.com/advice")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            $("#quote-id").text(data.slip.id);
+            $("#quote-text").text(data.slip.advice);
+            $("#button-circle").animate({ opacity: 0.5 });
+            setTimeout(function () {
+                $("#button-circle").animate({ opacity: 1 });
+            }, 1500)
+        });
+}
+
+const refreshButton = $("#button-circle");
+refreshButton.on("click", refresh);
+
+
+// Attribution
+
+const myImg = document.querySelector("#my-img");
+myImg.addEventListener("click", function() {
+    document.querySelector("#hidden").classList.toggle("hide");
+})
